4e98a5aad8945913a033a238602f523e1c1c6e02
Eixini <eixi16@gmail.com>
2022-02-28 00:58:30 +0300

    add new button

--- action.js
+++ action.js
@@ -1,21 +1,27 @@
 let buttons = document.getElementsByTagName('button');
 
 let headerButtonSettings = function(){
-    const buttonColor = 'blueviolet';
     
+    if(this.id === 'main-button') {
+        this.style.background = 'blueviolet';
+        window.open('index.hmtl');
+    }
     if(this.id === 'gallery-button') {
-        this.style.background = buttonColor;
+        this.style.background = 'blueviolet';
+        this.window.open('./gallery.hmtl');
     }
     else if(this.id === 'order-button'){
-        
+        this.style.background = 'blueviolet';
+        window.open('order.hmtl');
     }
     else if(this.id === 'contact-button'){
-        
+        this.style.background = 'blueviolet';
+        window.open('contacts.hmtl');
     }
 
 }
 
-for (var i = 0, len = buttons.length; i < len; i++){
+for (var i = 0, len = 3; i < len; i++){
     buttons[i].addEventListener('click', headerButtonSettings);
 }
 
